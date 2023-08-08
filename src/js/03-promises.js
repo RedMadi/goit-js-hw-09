6e2cb1ba299724bf5864d7746a155d0a3bb3da11
RedMadi <59694296+RedMadi@users.noreply.github.com>
2023-08-08 05:53:51 +0300

    3rd

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,34 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+const form = document.querySelector('.form');
+form.addEventListener('submit', onSubmit);
+
+function onSubmit(event) {
+  event.preventDefault();
+  let delay = Number(event.target.elements.delay.value);
+  let step = Number(event.target.elements.step.value);
+  let amount = Number(event.target.elements.amount.value);
+
+  for (let i = 1; i <= amount; i++) {
+    let position = i;
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    delay += step;
   }
+}
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
